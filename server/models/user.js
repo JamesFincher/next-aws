c63cab05bb47992659050308c0e5407f65838432
JamesFincher <heartandkarma@gmail.com>
2022-06-22 13:52:58 -0700

    updated schema more

--- server/models/user.js
+++ server/models/user.js
@@ -1,30 +1,42 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    minlength: 3,
-    maxlength: 255,
-    trim: true,
-    unique: true,
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 255,
+      trim: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 255,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      minlength: 3,
+      unique: true,
+      trim: true,
+    },
+    hashed_password: {
+      type: String,
+      required: true,
+    },
+    salt: String,
+    role: {
+      type: String,
+      default: 'subscriber',
+    },
+    resetPasswordLink: {
+      data: String,
+      default: '',
+    },
   },
-  name: {
-    type: String,
-    required: true,
-    minlength: 3,
-    maxlength: 255,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    minlength: 3,
-    unique: true,
-    trim: true,
-  },
-  hashed_password: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
